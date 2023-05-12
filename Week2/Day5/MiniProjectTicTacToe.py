b1325a65b023364ba8078ff7f57e720cf956e755
AmitAharon88 <aaharon88@gmail.com>
2023-05-12 14:04:09 +0300

    staring mini project

---
+++ Week2/Day5/MiniProjectTicTacToe.py
@@ -0,0 +1,33 @@
+# steps
+# display+board()
+#board = [
+    ['', 'x', ''],
+    ['', '', ''],
+    ['', '', ''],
+]
+# -->print board
+#player_input()
+#board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+]
+#--> determin which turn it is
+# max turns is 9
+# turn is odd --> x if turn is 1 --> x
+# turn is even --> o if turn is 2 --> o
+
+#input row and column
+# row:1
+#column: 2
+#check that user doesnt input a place that is already taken
+#check that columns and row are in the range of the board
+#if ask again (while loop)
+
+#board = [
+    ['', 'x', ''],
+    ['', '', ''],
+    ['', '', ''],
+]
+
+print new board
