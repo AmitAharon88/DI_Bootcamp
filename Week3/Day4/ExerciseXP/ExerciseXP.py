5781e9f43c856b67075587456d5a317ecbdbe3b2
AmitAharon88 <aaharon88@gmail.com>
2023-05-18 14:07:22 +0300

    adding part 2 of Exercise XP for W3D4

--- Week3/Day4/ExerciseXP/ExerciseXP.py
+++ Week3/Day4/ExerciseXP/ExerciseXP.py
@@ -1,7 +1,9 @@
+# Exercuse 1
+
 import random
 
 def get_words_from_file() :
-    with open("DI_Bootcamp/Week3/Day4/sowpods.txt", "r") as words:
+    with open("DI_Bootcamp/Week3/Day4/ExerciseXP/sowpods.txt", mode='r') as words:
         words_lst = words.read().splitlines()     # .splitlines() used to split str into list
         # print(words_lst)
         return words_lst
@@ -13,6 +15,7 @@ def get_random_sentence(length) :
         return random_sentence.lower()
 
 def main() :
+    print('This program will create a sentance for you. The length of that sentence will be based on the length you provide it.')
     s_length = int(input('How long do you want to sentence to be (between 2-20)? '))
     try :
         if s_length > 2 and s_length < 20 :
@@ -23,10 +26,35 @@ def main() :
         print('VALUE ERROR')
         print(error)
         
-        
+     
 get_words_from_file()
+# print(get_words_from_file()) 
 get_random_sentence(5)
 main()
 
-    
-    
+
+# Exercise 2
+
+import json
+
+sampleJson = """{ 
+   "company":{ 
+      "employee":{ 
+         "name":"emma",
+         "payable":{ 
+            "salary":7000,
+            "bonus":800
+         }
+      }
+   }
+}"""
+
+person_dict = json.loads(sampleJson)
+print(person_dict['company']['employee']['payable']['salary'])
+
+person_dict['company']['employee']['birth_date'] ='1988/01/08'
+print(person_dict)
+
+person_json = json.dumps(person_dict)
+print(person_json)
+
