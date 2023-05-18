e4ca7e2283a1b9838448f667712a64452b572cd6
AmitAharon88 <aaharon88@gmail.com>
2023-05-18 14:39:15 +0300

    submitting W3D5 DC-modules

---
+++ Week3/Day5/DailyChallenge/DailyChallengeModules.py
@@ -0,0 +1,15 @@
+import requests
+from time import time
+
+def load_time(url) :
+    start_time = time()  # number of second since the epoch
+    response = requests.get(url)
+    end_time = time()
+    time_passed = end_time - start_time
+    return time_passed
+
+print(load_time('https://www.coolthings.com/'))
+print(load_time('https://www.google.com/'))
+print(load_time('https://www.youtube.com/'))
+print(load_time('https://www.ynet.co.il/home/0,7340,L-8,00.html'))
+
