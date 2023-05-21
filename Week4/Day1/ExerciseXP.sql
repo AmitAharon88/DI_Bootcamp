7b37df7754772a1510888c06b6941937790fe87e
AmitAharon88 <aaharon88@gmail.com>
2023-05-21 13:58:48 +0300

    Submitting W4D1 exercise xp

---
+++ Week4/Day1/ExerciseXP.sql
@@ -0,0 +1,51 @@
+-- Database: Public
+
+-- DROP DATABASE IF EXISTS "Public";
+
+-- CREATE DATABASE "Public"
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'C'
+--     LC_CTYPE = 'C'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1
+--     IS_TEMPLATE = False;
+	
+-- Exercise 1 : Items And Customers
+
+-- CREATE TABLE items (
+-- 	item_id SERIAL PRIMARY KEY,
+-- 	item_name VARCHAR(100) NOT NULL,
+-- 	price SMALLINT NOT NULL
+-- )
+
+-- CREATE TABLE customers (
+-- 	customer_id SERIAL PRIMARY KEY,
+-- 	first_name VARCHAR(100) NOT NULL,
+-- 	last_name VARCHAR(100) NOT NULL
+-- )
+
+-- INSERT INTO items (item_name, price)
+-- VALUES
+-- ('Small Desk', 100),
+-- ('Large Desk', 300),
+-- ('Fan', 80)
+
+-- INSERT INTO customers (first_name, last_name)
+-- VALUES
+-- ('Greg', 'Jones'),
+-- ('Sandra', 'Jones'),
+-- ('Scott', 'Scott'),
+-- ('Trever', 'Green'),
+-- ('Melanie', 'Johnson')
+
+-- SELECT * FROM items
+-- SELECT * FROM items WHERE price>80
+-- SELECT * FROM items WHERE price<=300
+-- SELECT * FROM customers WHERE last_name = 'Smith'
+-- (Outcome is nothing as there is no customers with the name Smith)
+-- SELECT * FROM customers WHERE last_name = 'Jones'
+-- SELECT * FROM customers WHERE first_name != 'Scott'
+
+
