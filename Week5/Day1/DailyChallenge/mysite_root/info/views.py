60df4923eeef715e85acbd7686b158ea3cc00cc8
AmitAharon88 <aaharon88@gmail.com>
2023-05-29 10:03:59 +0300

    change to W4D1 DC

--- Week5/Day1/DailyChallenge/mysite_root/info/views.py
+++ Week5/Day1/DailyChallenge/mysite_root/info/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
-from people import *
 # Create your views here.
-filename = 'Day1/DailyChallenge/mysite_root/info/people.py'
-
+from people import people
 
 with open(filename, "r") as p:
     p.read()
 
 def all_people_view (request) :
-    people = people.objects.order_by('age')
-    context = {'people': people}
-    return render(request, 'people.html', context)
+    people_sorted = sorted(people, key =  lambda person: person['age'])
+    context = {'people': people_sorted}
+    return render(request, 'allPeople.html', context)
 
-def person_view (request, id) :
+def person_view (request, id: int) :
     person_selected = ''
     for i in people :
         if i['id'] == id :
             person_selected = i
-    return render (request, 'singlePerson.html', person_selected)
+            break
+        context = {'person_instance': person_selected}
+    return render (request, 'singlePerson.html', context)
