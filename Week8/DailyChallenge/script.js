9fadb49e631b282cecb3f9761bd82a16234ab8a4
AmitAharon88 <aaharon88@gmail.com>
2023-06-26 13:49:00 +0300

    submitting changes to w8d1 dc

--- Week8/DailyChallenge/script.js
+++ Week8/DailyChallenge/script.js
@@ -46,3 +46,26 @@ function addStory(noun, adjective, person, verb, place) {
 
 
 // OTher way to do it (better)- from class
+// const myform = document.forms.libform
+// myform.addEventListener("submit", getValues);
+
+// function getValues(event) {
+//     event.preventDefault();
+//     let objValues = {}
+//     const allInputs = event.target.querySelectorAll("input");
+//     for (let inp of allInputs) {
+//         if (inp.value === "") {
+//             alert("fill the form - missing element")
+//             return;
+//         }
+//         objValues[inp.id] = inp.value;
+//     }
+//     showStory(objValues)
+// }
+
+// function showStory (objValues) {
+//     const spanElement = document.getElementById("story");
+//     const text = `The ${objValues["noun"]} is ${objValues["verb"]} with ${objValues["person"]}`
+//     const textNode = document.createTextNode(text)
+//     spanElement.appendChild(textNode)
+// }
