3b797b08536cb8fd0c70712e882699c22be5fc85
AmitAhron88 <aaharon88@gmail.com>
2023-07-09 16:10:48 +0300

    submitting week10 dc

---
+++ Week10/DailyChallenge/script.js
@@ -0,0 +1,9 @@
+// Part I:
+const number = require('./main.js');
+const b = 5;
+
+console.log(number + b);
+
+// Part II: --> to do tomorrow when we learn http
+
+// Part III: --> to do tomorrow when we learn http
