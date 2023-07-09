3f6fe65179143f72a0a4e04ee4fd1d9dc0e4305c
AmitAhron88 <aaharon88@gmail.com>
2023-07-09 14:31:44 +0300

    submitting week10 xp

---
+++ Week10/ExerciseXP/npm-node/main.js
@@ -0,0 +1,24 @@
+// Exercise 1 : Hello Node.Js
+
+// const a = 5;
+// const b = 10;
+// const sum = a + b
+// console.log(`Hello you are ${sum}`)
+
+
+
+
+// Exercise 2 : Hello Node.Js #2
+// Done
+
+// Exercise 3 : Nodemon
+const a = 20;
+const b = 10;
+let x;
+const c = 6;
+
+x = a + b;
+
+console.log(x);
+
+console.log(x + c);
