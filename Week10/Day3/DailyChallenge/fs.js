de82e60605812083ac85dbe4281beeac106c5ee1
AmitAhron88 <aaharon88@gmail.com>
2023-07-11 22:28:01 +0300

    submitting xp

---
+++ Week10/Day3/DailyChallenge/fs.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+// Read synch method
+fs.readFile('RightLeft.txt', 'utf-8', (err, data) => {
+    if (err) {
+        return console.log(err);
+    }
+    totalSteps(data);
+    minusPosition(data);
+});
+
+function totalSteps(data) {
+    const dataArray = data.split('');
+    let position = 0;
+    for (let symbol of dataArray) {
+        symbol === '>' ? position += 1 : position -= 1;
+    }
+    console.log(position);
+};
+
+function minusPosition(data) {
+    const dataArray = data.split('');
+    let steps = 0;
+    let currentPosition = 0;
+
+    for (let symbol of dataArray) {
+       symbol === '>' ? currentPosition += 1 : currentPosition -= 1;
+       steps++
+
+       if (currentPosition === -1) {
+        console.log(steps)
+        break
+        };
+    }; 
+};
